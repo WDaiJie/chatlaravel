76a1d1d5f20126e2c7d35ebded929d600940f26b
W,Dai-Jie <52863109+WDaiJie@users.noreply.github.com>
2019-08-27 11:33:13 +0800

    Add files via upload

--- chat_laravel/resources/views/profile/job.blade.php
+++ chat_laravel/resources/views/profile/job.blade.php
@@ -1,56 +1,86 @@
 @extends('profile.master')
 
 @section('content')
+<style>
+.jobDetails h4{
+    border:1px solid #52075f; 
+    width:60%;
+    padding:5px; 
+    margin:0 auto; 
+    text-align:center; 
+    color:#745ab3
+}
+.jobDetails .job_company{
+    padding-bottom:10px; 
+    border-bottom:1px solid #ddd; 
+    margin-top:20px
+}
+.jobDetails .job_point{
+    color:#030388; 
+    font-weight:bold
+}
+
+.jobDetails .email_link{
+    padding:5px; 
+    border:1px solid #d20d0d; 
+    color:#030388
+}
+</style>
 <div class="container">
     <ol class="breadcrumb">
         <li class="breadcrumb-item "><a href="{{url('/home ')}}">Home</a></li>
         <li class="breadcrumb-item" ><a href="{{url('/profile')}}/{{Auth::user()->slug}}">Profile</a></li>
         <li class="breadcrumb-item" ><a href="{{url('/editProfile')}}">Edit Profile</a></li>
-        <li class="breadcrumb-item" ><a href="#">Jobs</a></li>
+        <li class="breadcrumb-item" ><a href="{{url('/jobs')}}">Jobs</a></li>
+        <li class="breadcrumb-item" ><a href="#">{{$jobs[0]->job_title}}</a></li>
    </ol>
     <div class="row justify-content-center">
         @include('profile.sidebar')
         <div class="col-md-9">
             <div class="card">
-            <div class="card-header">
-                {{ucwords(Auth::user()->name)}}, may you interested in these Jobs<br>
-                <a href="{{url('jobs')}}">ALL Jobs</a>
-            </div>
-                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                            <div class="col-sm-12 col-md-12">
-                            @if (session()->has('data'))
-                                <p class="alert alert-danger">{!! (session()->get('data')) !!}</p>                                                  
-                            @endif
-                                @foreach($jobs as $jobkey)
-                                <div class="col-md-12">
-                                   <div class="col-md-4">
-                                        <a href="{{url('/job/')}}/{{$jobkey->id}}">
-                                            <div class="thumbnail">
-                                                <div class="cpation">
-                                                    <p>
-                                                        <b>Job Title:</b> {{ucwords($jobkey->job_title)}}<br>
-                                                        <b>Email:</b>{{ucwords($jobkey->contact_email)}}<br>
-                                                        <b>Skills:</b> {{ucwords($jobkey->skills)}}<br>
-                                                        <b>Requirements:</b>{{ucwords($jobkey->requirements)}}<br>
-                                                        <b>company:</b> {{ucwords($jobkey->company_id)}}<br>
-                                                    </p>
-                                        
-                                                </div>
-                                            </div>
-                                        </a>
-                                   </div>
-                                </div>
-                                @endforeach
-                            </div>     
-                        </div>
+                <div class="card-header">
+                    {{ucwords(Auth::user()->name)}}, may you interested in these Jobs<br>
+                    <a href="{{url('jobs')}}"><b style="font-size:30px;">See ALL Job</b></a>
                 </div>
-            </div>
+                     <div class="card-body">
+                        <div class="col-sm-12 col-md-12 jobDetails">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            <div class="col-sm-12 col-md-12">
+                                @if (session()->has('data'))
+                                <p class="alert alert-danger">{!! (session()->get('data')) !!}</p>                                                  
+                                @endif
+                                @foreach($jobs as $job)
+                                <h4><b>{{ucwords($job->name)}} need {{$job->job_title}}</b></h4>
+                                <div class="row job_company">
+                                    <div class="col-md-2 pull-left">
+                                        <img src="{{Config::get('app.url')}}/public/img/{{$job->image}}" class="img-rounded" style="width:140px; height:140px; margin:10px; border:2px solid #ddd; padding:5px">
+                                    </div>
+                                    <div class="col-md-10 pull-right" style="padding-left:100px">
+                                        <p style="font-size:18px;">Name :{{ucwords($job->name)}}</p>
+                                        <p style="font-size:18px;">Gender :{{ucwords($job->gender)}}</h3>
+                                        <p style="font-size:18px;">Phone :{{ucwords($job->phone)}}</h3>
+                                        <p style="font-size:18px;">Email :{{ucwords($job->email)}}</h3>
+                                    </div>
+                                </div>
+                                <div class="col-md-12" style="padding-top:15px">
+                                    <h3 class="job_point">Requirements: </h3><p>{{$job->requirements}}</p>
+                                </div>
+                                <div class="col-md-12" style="padding-top:2px">
+                                        <h3 class="job_point">Skills:</h3><p>{{$job->skills}}</p>
+                                </div>
+                                <div class="col-md-12" style="padding-top:2px">
+                                    <h3 class="job_point">How to Apply: </h3><p>Please send your awesome CV and PortFolio to email:
+                                    <a href="mailto:{{$job->contact_email}}" class="email_link">{{$job->contact_email}}</a></p>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>     
+                    </div>
+            </div>   
          </div>
     </div>
 </div>
--- chat_laravel/resources/views/profile/jobs.blade.php
+++ chat_laravel/resources/views/profile/jobs.blade.php
@@ -1,46 +1,73 @@
 @extends('profile.master')
 
 @section('content')
+<style>
+    .jobDiv .company_pic{
+        width:50px; 
+        height:50px; 
+        margin:5px
+    }
+    .jobDiv{
+        border:1px solid #ddd; 
+        margin:10px; 
+        width:30%; 
+        float:left; 
+        padding:10px; 
+        color:#000
+    }
+    .jobbarli{
+        padding-left:10px; 
+        padding-right:5px;
+        padding-top:2px;
+        padding-bottom:2px
+        text-align:justify;
+        display: block;
+        width: 100%;
+    }
+</style>
 <div class="container">
     <ol class="breadcrumb">
         <li class="breadcrumb-item "><a href="{{url('/home ')}}">Home</a></li>
         <li class="breadcrumb-item" ><a href="{{url('/profile')}}/{{Auth::user()->slug}}">Profile</a></li>
         <li class="breadcrumb-item" ><a href="{{url('/editProfile')}}">Edit Profile</a></li>
-        <li class="breadcrumb-item" ><a href="#">Jobs</a></li>
+        <li class="breadcrumb-item" ><a href="{{url('/jobs')}}">Jobs</a></li>
    </ol>
     <div class="row justify-content-center">
         @include('profile.sidebar')
         <div class="col-md-9">
             <div class="card">
-            <div class="card-header">
-                {{ucwords(Auth::user()->name)}}, may you interested in these Jobs<br>
-            </div>
-                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
+                
+                    <div class="card-header">
+                        <h4><span style="color:#4905cc">{{ucwords(Auth::user()->name)}}</span>,Jobs you may be interested in</h4>Any location Selected industries:  Any industry Selected company size range:  1 to 1,000 employees
+                    </div>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
                             <div class="col-sm-12 col-md-12">
                             @if (session()->has('data'))
                                 <p class="alert alert-danger">{!! (session()->get('data')) !!}</p>                                                  
                             @endif
-                                @foreach($jobs as $jobkey)
-                                <div class="col-md-12">
-                                   <div class="col-md-4">
-                                        <a href="{{url('/job/')}}/{{$jobkey->id}}">
-                                            <div class="thumbnail">
-                                                <div class="cpation">
-                                                    <p>{{ucwords($jobkey->job_title)}}</p>
+                            @foreach($jobs as $jobkey)
+                                <div class="jobDiv">
+                                    <a href="{{url('job')}}/{{$jobkey->id}}">
+                                        <div class="caption">
+                                            <div class="jobbarli"><img src="{{Config::get('app.url')}}/public/img/findjob.png"width="25" style="margin:5px"/> {{$jobkey->job_title}} </div>
+                                            <div class="jobbarli"> <img src="{{Config::get('app.url')}}/public/img/{{$jobkey->image}}" width="25" style="margin:px"/>  {{ucwords($jobkey->name)}}</div>
+                                    </a>
+                                            <?php $skills = explode(',',$jobkey->skills)?>
+                                                @foreach($skills as $skill)
+                                                <div style="background-color:#2e2569; color:#fff; margin-top:5px; border-radius:10px; width:100%; float:left; padding:3px 15px 3px 15px">{{$skill}}
                                                 </div>
-                                            </div>
-                                        </a>
-                                   </div>
-                                </div>
-                                @endforeach
-                            </div>     
-                        </div>
+                                                @endforeach
+                                                <a href="{{url('job')}}/{{$jobkey->id}}" style="margin-top:10px; width:100%" class="btn btn-primary">View details</a>
+                                            
+                                        </div>
+                                </div>                       
+                            @endforeach
+                    </div>
                 </div>
             </div>
          </div>
