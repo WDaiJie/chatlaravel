857d2c28eaaa336e9f07d9e4c62ef694a90f9187
W,Dai-Jie <52863109+WDaiJie@users.noreply.github.com>
2019-08-28 01:02:43 +0800

    08_28upload

--- chat_laravel/app/Posts.php
+++ chat_laravel/app/Posts.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     public $table = "posts";     
+    //create relation users and posts 
+    public function user(){
+        return $this->belongsTo(user::class);
+    }
+    public function likes(){
+        return $this->hasMany(likes::class,'post_id');
+    }
+    public function comments(){
+        return $this->hasMany(comments::class,'post_id');
+    }
 
 }
