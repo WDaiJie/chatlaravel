b9008676163bf2594aebdc90026082c273c9db84
W,Dai-Jie <52863109+WDaiJie@users.noreply.github.com>
2019-08-28 22:46:48 +0800

    08-28-upload

--- chat_laravel/app/Http/Controllers/PostsController.php
+++ chat_laravel/app/Http/Controllers/PostsController.php
@@ -21,22 +21,21 @@ class PostsController extends Controller
     }
     public function addnewPost(Request $request){
         $uid=Auth::user()->id;
-         $content = $request->content;
-         $creatPost=DB::table('posts')
-         ->insert(['content'=>$content,'user_id'=>$uid,'status'=>0,'created_at'=>date("Y-m-d H:i:s"),'updated_at'=>date("Y-m-d H:i:s")]);
-         if($creatPost){
+        $content = $request->content;
+        $creatPost=DB::table('posts')
+        ->insert(['content'=>$content,'user_id'=>$uid,'status'=>0,'created_at'=>date("Y-m-d H:i:s"),'updated_at'=>date("Y-m-d H:i:s")]);
+        if($creatPost){
             // $posts_data_new= DB::table('posts')
             // ->leftJoin('profile','profile.user_id','posts.user_id')
             // ->leftJoin('users','users.id','posts.user_id')
             // ->orderBy('posts.created_at','desc')->take(4)
             // ->select('posts.*','users.name','users.gender','users.slug','users.image','profile.city','profile.country','profile.about')
             // ->get();
-            $posts_data_new=posts::with('user','likes')
+            $posts_data_new=posts::with('user','likes','comments')
             ->orderBy('posts.created_at','desc')
             ->get();
-
-            return $posts_data_new;
-         }
+            return $posts_data_new;   //return all posts same as before
+        }
     }
     public function show(){
         // $posts_data= DB::table('posts')
@@ -76,5 +75,43 @@ class PostsController extends Controller
     public function likesShow(){
         $likes=likes::all();
         return $likes;
+    }
+    public function addComment(Request $request){
+        $loggedid=Auth::user()->id;
+        $post_id=$request->id;
+        $comments = $request->comment;
+        $commentInsert=DB::table('comments')
+        ->insert([
+            'comment'=>$comments,
+            'user_id'=>$loggedid,
+            'post_id'=>$post_id,
+            'created_at'=>date("Y-m-d H:i:s"),
+            'updated_at'=>date("Y-m-d H:i:s")
+            ]);
+        if($commentInsert){
+            return posts::with('user','likes','comments')->orderBy('created_at','desc')->get(); 
+        }
+    }
+    public function saveImage(Request $request){
+        $image=$request->get('image');
+        $explodedImg=explode(",",$image);  // ',' cutting
+        //extention
+        if(str_contains($explodedImg[0],'gif')){  //if explodedImg has gif?
+            $ext='gif';
+        }else if(str_contains($explodedImg[0],'png')){
+            $ext='png';
+        }else{
+            $ext='jpg';
+        }
+        //decode    Img64
+        $decode=base64_decode($explodedImg[1]);
+        $filename=str_random(10).".".$ext;
+        //path of your local folder
+        $Imgpath=public_path()."/img/".$filename;  //public path to the directory
+        //upload image to your path
+        if(file_put_contents($Imgpath,$decode)){ // writes a string to a file.
+            echo "file scusses ".$filename;
+
+        }  
     }
 }
